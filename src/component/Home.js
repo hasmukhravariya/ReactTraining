e9a9d29b8636b6f96cc3bf2400cb8a866eab8cde
hasmukh <hasmukh16000@gmail.com>
2021-07-01 17:25:17 +0530

    added email, phone, pass and its validation

---
+++ src/component/Home.js
@@ -0,0 +1,11 @@
+
+import {Link} from "react-router-dom";
+
+
+export default function Home() {
+  return (
+    <div className='center'>
+      <Link to="/Form" className="button">Open</Link>
+    </div>  
+  );
+}
